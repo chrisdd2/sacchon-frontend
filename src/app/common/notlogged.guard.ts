118d2b60a2c0f969e25e4f993e59d613da1ebb2d
Christos Damianidis <chris.damianidis2@gmail.com>
2021-04-14 20:07:24 +0300

    placerholder home page

--- src/app/common/notlogged.guard.ts
+++ src/app/common/notlogged.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
@@ -7,11 +7,17 @@ import { AuthService } from '../services/auth.service';
   providedIn: 'root'
 })
 export class NotLoggedGuard implements CanActivate {
-  constructor(private auth:AuthService){}
+  constructor(private auth:AuthService,
+              private router:Router){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      return !this.auth.isLoggedIn();
+      // redirect to role home if logged
+      if ( this.auth.isLoggedIn() ){
+        this.router.navigateByUrl(this.auth.getHomeRoute());
+        return false;
+      }
+      return true;
   }
   
 }
