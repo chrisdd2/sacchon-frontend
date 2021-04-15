2a9fb358257999c0f83f88884eb64fc50896b320
Christos Damianidis <chris.damianidis2@gmail.com>
2021-04-15 17:41:57 +0300

    signup initial steps

--- src/app/authentication/signup/signup.component.ts
+++ src/app/authentication/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ɵangular_packages_forms_forms_bh } from '@angular/forms';
 
 @Component({
   selector: 'sacchon-signup',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  form:FormGroup;
+  account_type:string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.form = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      password: new FormControl(),
+      passwordConfirm: new FormControl()
+    })
+    this.account_type='';
+  }
+
+
+  onSubmit(){
+    console.log(this.form.controls.firstName.value);
   }
 
 }
